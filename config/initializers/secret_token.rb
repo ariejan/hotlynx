3ad17c659c8f43347440dd3425c7df7185d09de9
John van Arkelen <johnvanarkelen@gmail.com>
2013-02-08 08:30:39 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotlynx::Application.config.secret_token = '716c7b934a2af2baf00d80cbaccdbc81e7789c9528a856cae12dd57884249003be5dcb17da706a15b329145fecfaacde536717dbd4a324c53afbd4cc3aa6d7d2'
